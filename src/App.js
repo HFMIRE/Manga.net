1e68e5da9a4c12fbbe27ed1ee5fd4d199bc24e20
HFMIRE <huda.f.mire@gmail.com>
2021-07-14 15:03:47 +0100

    updating tags

--- src/App.js
+++ src/App.js
@@ -23,3 +23,13 @@ function App() {
   );
 }
 export default App;
+
+// step 1 - form - upload image + rating  - different componet
+// are okay with details - comfirming with modal - pop up are sure this correct + details with the label
+// land on the homepage - is yes or no - go back prefilled form + can edit
+// adding it to the carousel - through data
+// form component - using as a state mangement - redux
+
+// adding ts
+// adding state mangement - redux
+// race condontional -
